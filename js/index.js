ae4797536131b2acf04372c4a79d5b5d37ad0ec4
Guanghua.chen <guanghua.chen@alibaba-inc.com>
2018-05-31 23:28:47 +0800

    delete weread links

--- js/index.js
+++ js/index.js
@@ -253,11 +253,11 @@ if (USER_AGENT.match(/MSIE 8./) || USER_AGENT.match(/MSIE 7./) || USER_AGENT.mat
                 } else if (GLOBAL.DEVICE_TYPE === 1) {
                     ax.style.display = 'none';
                     ai.style.display = 'block';
-                    ah.href = 'https://i.weread.qq.com/download?from=Website&type=iOS';
+                    //ah.href = 'https://i.weread.qq.com/download?from=Website&type=iOS';
                 } else if (GLOBAL.DEVICE_TYPE === 2) {
                     ax.style.display = 'none';
                     ag.style.display = 'block';
-                    ah.href = 'https://i.weread.qq.com/download?from=Website&type=Android';
+                    //ah.href = 'https://i.weread.qq.com/download?from=Website&type=Android';
                 }
                 if (GLOBAL.DEVICE_TYPE) {
                     M.style.backgroundImage = 'url("images/mobile_function4_table_background_4x.png")';
